7231f4cc9391f4babd55e2853dd4e1cf1200a5d5
slayeroak <roakley1@villanova.edu>
2020-01-14 13:37:47 -0500

    created 4 character objects with name img location hp ap and cap

---
+++ assets/javascript/game.js
@@ -0,0 +1,32 @@
+// game character objects
+var yoda = {
+    name: "Yoda",
+    img_location: "",
+    health_points: 300,
+    attack_power: 10,
+    counter_attack_power: 10,
+};
+
+var lukeSkywalker = {
+    name: "Luke Skywalker",
+    img_location: "",
+    health_points: 135,
+    attack_power: 15,
+    counter_attack_power: 8,
+};
+
+var darthVader = {
+    name: "Darth Vader",
+    img_location: "",
+    health_points: 140,
+    attack_power: 7,
+    counter_attack_power: 15,
+};
+
+var darthMaul = {
+    name: "Darth Maul",
+    img_location: "",
+    health_points: 200,
+    attack_power: 3,
+    counter_attack_power: 20,
+};
