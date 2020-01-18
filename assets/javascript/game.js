1bef66c2df0cd2e941d4111db3ce2ffc705792f7
slayeroak <roakley1@villanova.edu>
2020-01-18 14:57:29 -0500

    added a feature to the onclick that will grab luke skywalkers api data from the star wars api, is stable. need to figure out how to dynamically grab character numbers from star wars api since they are numbered...

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -42,10 +42,69 @@ var noDefender = true;
 var gameOver = false;
 var backgroundImage
 
- 
+//  url
+var swURL = "https://swapi.co/api/";
 
 
 // Game Functions
+
+function displayMovieInfo() {
+
+    var movie = $(this).attr("data-name");
+    var queryURL = swURL + "people/1/";
+
+    // Creating an AJAX call for the specific movie button being clicked
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+        var res = response;
+        console.log(res);
+
+    //   // Creating a div to hold the movie
+    //   var movieDiv = $("<div class='movie'>");
+
+    //   // Storing the rating data
+    //   var rating = response.Rated;
+
+    //   // Creating an element to have the rating displayed
+    //   var pOne = $("<p>").text("Rating: " + rating);
+
+    //   // Displaying the rating
+    //   movieDiv.append(pOne);
+
+    //   // Storing the release year
+    //   var released = response.Released;
+
+    //   // Creating an element to hold the release year
+    //   var pTwo = $("<p>").text("Released: " + released);
+
+    //   // Displaying the release year
+    //   movieDiv.append(pTwo);
+
+    //   // Storing the plot
+    //   var plot = response.Plot;
+
+    //   // Creating an element to hold the plot
+    //   var pThree = $("<p>").text("Plot: " + plot);
+
+    //   // Appending the plot
+    //   movieDiv.append(pThree);
+
+    //   // Retrieving the URL for the image
+    //   var imgURL = response.Poster;
+
+    //   // Creating an element to hold the image
+    //   var image = $("<img>").attr("src", imgURL);
+
+    //   // Appending the image
+    //   movieDiv.append(image);
+
+    //   // Putting the entire movie above the previous movies
+    //   $("#movies-view").prepend(movieDiv);
+      
+    });
+}
 
 // determine which character that player selects
 function findCharacter(name){
@@ -102,9 +161,12 @@ $(document).ready(function(){
         console.log(this);
         user_hp = user_char.health_points;
         user_att_power = user_char.attack_power;
+
+        displayMovieInfo();
    
         //print the enemies, define print enemeny above
         printEnemy(this);
+        
 
         // change game background image to battle stage image
         backgroundImage = "assets/images/swbattlestage.jpg";
@@ -133,5 +195,4 @@ $(document).ready(function(){
     $("#attackButton").on("click"), function () {
         // if no defender, if game is not over
     }
-
-})
+});
